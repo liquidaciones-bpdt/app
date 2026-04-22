2a5146d06c015b2830fbf77a3179561cbd4032d8
Liquidaciones <liquidaciones.fape.qs@gmail.com>
2026-04-22 13:57:57 -0500

    Create config.js

---
+++ asistente/config.js
@@ -0,0 +1,10 @@
+/**
+ * CONFIGURACIÓN GLOBAL
+ * Centralización del acceso al sistema.
+ */
+const CONFIG = {
+    // Reemplaza esto con tu URL de "Implementación como Aplicación Web" (Web App URL)
+    API_URL: "https://script.google.com/macros/s/AKfycbz_XXXXXXXXXXXX/exec"
+};
+
+export default CONFIG;

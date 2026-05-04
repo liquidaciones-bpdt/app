4b15284b1ba030a3f026506507c811f9b00a713c
Liquidaciones <liquidaciones.fape.qs@gmail.com>
2026-05-03 21:25:58 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+/** CONFIGURACIÓN FRONTEND — GitHub Pages + Apps Script Web App */
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbzGm8Zp2oeOAHLehmCADf1LLN-WdOJH9lseHBcmMjORLWfBt6JoppAjWgRSs7CTZ42o/exec"
+};
+export default CONFIG;

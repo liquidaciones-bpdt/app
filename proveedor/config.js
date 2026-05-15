70f45b603a982d50a59d8ff8ffde787f00f8e89b
Liquidaciones <liquidaciones.fape.qs@gmail.com>
2026-05-15 01:45:17 -0500

    Update API_URL in config.js

--- proveedor/config.js
+++ proveedor/config.js
@@ -4,7 +4,7 @@
  */
 const CONFIG = {
     // Reemplaza esto con tu URL de "Implementación como Aplicación Web" (Web App URL)
-    API_URL: "https://script.google.com/macros/s/AKfycbyydzCFSlMKWUTsIUf_DM74JCYadj_YBM87T9syLOkFpTUJdulem4Lfymuzi5UyxUrT/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbwE_lmNw89JMRklpkz-VHJ48OgFrVc7KwL1q1-mimYFL8UQ68FSfcEBHQaA6YGMbeum/exec"
 };
 
 export default CONFIG;
